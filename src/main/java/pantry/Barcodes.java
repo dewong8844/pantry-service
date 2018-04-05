31444de6ad0c8c270ba3cb258b9ff8e9139591b0
Dennis Wong <dewong4@gmail.com>
2018-04-04 22:38:51 -0700

    Update, delete, product api return barcode as well

--- src/main/java/pantry/Barcodes.java
+++ src/main/java/pantry/Barcodes.java
@@ -1,5 +1,7 @@
 package pantry;
 
+import java.util.Set;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -8,6 +10,7 @@ import redis.clients.jedis.Jedis;
 public class Barcodes {
 	private static final String barcodesIndex = "barcodes:_id";
 	private static final String barcodesLookupByValue = "barcodes:lookup:value";
+	private static final String barcodesLookupByProductid = "barcodes:lookup:productid";
 	private static final String barcodesTable = "barcodes:";
 	private static final String hostname = "localhost";
 
@@ -18,18 +21,21 @@ public class Barcodes {
     public static String getAll() {
     	System.out.println("Query all barcodes");
         Jedis jedis = new Jedis(hostname);
-        String res = jedis.get(barcodesIndex);
-        if (res == null) {
-        	// log error - key doesn't exist in redis
-        	jedis.close();
-        	return "";
+        Set<String> keys = jedis.keys("barcodes:[0-9]*");
+        String res = "[";
+        int count = (keys != null)? keys.size() : 0;
+        if (count > 0) {     
+            for (String key : keys) {
+            	String val = jedis.get(key);
+            	if (val == null) {
+            		// log error - key doesn't exist in redis
+            		jedis.close();
+            		return "";
+            	}
+            	count--;
+            	res += val + (count > 0? ", " : "");
+            }
         }
-        int count = Integer.parseInt(res);
-        res = "[";
-        for (int i = 1; i < count; i++) {
-           res += get(Integer.toString(i)) + ", ";
-        }
-        res += get(Integer.toString(count));
         jedis.close();
         return res + "]";
     }
@@ -52,17 +58,46 @@ public class Barcodes {
      * @param String id
      * @return String, json object containing barcode :id in DB
      */
-    public static String getByValue(String value) {
-    	System.out.println("Query barcode value: " + value);
+    public static Barcode getByValue(String value) {
+    	System.out.println("Query barcode by value: " + value);
         Jedis jedis = new Jedis(hostname);
-        String res = null;
+        Barcode barcode = null;
         
-        String id = jedis.hget(barcodesLookupByValue, value);
-        if (id != null) {
-        	res = jedis.get(barcodesTable + id);
-        }
-        jedis.close();
-        return res;
+        try {
+        	String res = null;
+        	Gson gson = new GsonBuilder().create();
+        	String id = jedis.hget(barcodesLookupByValue, value);
+        	if (id != null) {
+        		res = jedis.get(barcodesTable + id);
+        	}
+        	barcode = gson.fromJson(res, Barcode.class);
+        	jedis.close();
+        } catch (Exception e) {
+    		e.printStackTrace();
+    		System.err.println(Barcodes.class.getSimpleName() + e.getMessage());
+    	}
+        return barcode;
+    }
+    
+    public static Barcode getByProductid(String productid) {
+    	System.out.println("Query barcode by productid: " + productid);
+        Jedis jedis = new Jedis(hostname);
+        Barcode barcode = null;
+        
+        try {
+        	String res = null;
+        	Gson gson = new GsonBuilder().create();
+        	String id = jedis.hget(barcodesLookupByProductid, productid);
+        	if (id != null) {
+        		res = jedis.get(barcodesTable + id);
+        	}
+        	barcode = gson.fromJson(res, Barcode.class);
+        	jedis.close();
+        } catch (Exception e) {
+    		e.printStackTrace();
+    		System.err.println(Barcodes.class.getSimpleName() + e.getMessage());
+    	}
+        return barcode;
     }
 
     /**
@@ -70,13 +105,14 @@ public class Barcodes {
      * @param barcodeJson HTTP Request Body with new barcode information
      * @return String, json object containing product with new ID in DB
      */
-    public static String create(String barcodeJson) {
+    public static Barcode create(String barcodeJson) {
         System.out.println("Create barcode: " + barcodeJson); 
         String outputJson = null;
+        Barcode barcode = null;
         
         try {
         	Gson gson = new GsonBuilder().create();
-        	Barcode barcode = gson.fromJson(barcodeJson, Barcode.class);
+        	barcode = gson.fromJson(barcodeJson, Barcode.class);
         	// connect to redis server
             Jedis jedis = new Jedis(hostname);
             if (jedis.get(barcodesIndex) == null ) {
@@ -90,6 +126,7 @@ public class Barcodes {
             outputJson = gson.toJson(barcode);
             jedis.set(barcodesTable + id, outputJson);
             jedis.hset(barcodesLookupByValue, barcode.getValue(), id);
+            jedis.hset(barcodesLookupByProductid, Long.toString(barcode.getProductid()), id);
             jedis.close();                       
         	 
        } catch (Exception e) {
@@ -97,29 +134,36 @@ public class Barcodes {
         	System.err.println(Barcodes.class.getSimpleName() + e.getMessage());
         }
         
-        return outputJson;
+        return barcode;
     }
 
     /**
      * PUT /barcode/:id
      * @param String id
      * @param productJson HTTP Request Body with new product information
-     * @return String, json object containing update product in DB
+     * @return Barcode, json object containing update product in DB
      */
-    public static String update(String id, String barcodeJson) {
+    public static Barcode update(String id, String barcodeJson) {
         System.out.println("Update barcode: " + id);
         String outputJson = null;
+        Barcode barcode = null;
         Jedis jedis = new Jedis(hostname);
         String res = jedis.get(barcodesTable + id);
         if (res != null) {
         	// update only if id exists
-        	Gson gson = new GsonBuilder().create();
-        	Barcode barcode = gson.fromJson(barcodeJson, Barcode.class);
+        	Gson gson = new GsonBuilder().create();      	
+        	barcode = gson.fromJson(barcodeJson, Barcode.class);
+        	barcode.setBarcodeId(Integer.parseInt(id));
         	outputJson = gson.toJson(barcode);
         	jedis.set(barcodesTable + id, outputJson);
+        	Barcode oldbarcode = gson.fromJson(res, Barcode.class);
+        	jedis.hdel(barcodesLookupByValue, oldbarcode.getValue());
+        	jedis.hset(barcodesLookupByValue, barcode.getValue(), id);
+        	jedis.hdel(barcodesLookupByProductid, Long.toString(oldbarcode.getProductid()));
+        	jedis.hset(barcodesLookupByProductid, Long.toString(barcode.getProductid()), id);
         }
         jedis.close();
-        return outputJson;
+        return barcode;
     }
     
     /**
@@ -137,10 +181,37 @@ public class Barcodes {
        		// update only if id exists
        		Gson gson = new GsonBuilder().create();
        		Barcode barcode = gson.fromJson(barcodeJson, Barcode.class);
+       		barcode.setBarcodeId(Integer.parseInt(id));
        		outputJson = gson.toJson(barcode);
        		jedis.set(barcodesTable + id, outputJson);
        	}
         jedis.close();
         return outputJson;
+    }
+    
+    /**
+     * DELETE /barcode/:id
+     * @param String id
+     * @return String, json object containing {"result": "Ok" or "Failed"}
+     */
+    public static String delete(String id) {
+    	System.out.println("Delete barcode: " + id);
+        Jedis jedis = new Jedis(hostname);                
+        String res = jedis.get(barcodesTable + id);
+        if (res != null) {
+        	Gson gson = new GsonBuilder().create();
+        	Barcode barcode = gson.fromJson(res, Barcode.class);
+        	jedis.hdel(barcodesLookupByValue, barcode.getValue());
+        	jedis.hdel(barcodesLookupByProductid, Long.toString(barcode.getProductid()));
+        	Long cnt = jedis.del(barcodesTable + id);
+        	if (cnt == 1) {
+        		// how to report result?
+        		res = "{\"result\": \"Ok\"}";
+        	} else {
+        		res = "{\"result\": \"Failed\"}";
+        	}
+        }
+        jedis.close();
+        return res;
     }
 }
--- src/main/java/pantry/Products.java
+++ src/main/java/pantry/Products.java
@@ -1,9 +1,13 @@
 package pantry;
 
-import redis.clients.jedis.Jedis;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import redis.clients.jedis.Jedis;
 
 public class Products {
 	private static final String productsIndex = "products:_id";
@@ -12,52 +16,65 @@ public class Products {
 
 	/**
 	 *  GET /product
-	 * @return String, json array containing all products in DB
+	 * @return List of Product objects
 	 */
-    public static String getAll() {
+    public static List<Product> getAll() {
     	System.out.println("Query all products");
         Jedis jedis = new Jedis(hostname);
-        String res = jedis.get(productsIndex);
-        if (res == null) {
-        	// log error - key doesn't exist in redis
-        	jedis.close();
-        	return "";
+        Set<String> keys = jedis.keys("products:[0-9]*");
+        List<Product> res = new ArrayList<Product>();
+    	Gson gson = new GsonBuilder().create();
+    	
+        if (keys != null && keys.size() > 0) {     
+            for (String key : keys) {
+            	String val = jedis.get(key);
+            	if (val != null) {
+            		Product product = gson.fromJson(val, Product.class);
+            		res.add(product);            
+            	}
+            }
         }
-        int count = Integer.parseInt(res);
-        res = "[";
-        for (int i = 1; i < count; i++) {
-           res += get(Integer.toString(i)) + ", ";
-        }
-        res += get(Integer.toString(count));
-        jedis.close();
-        return res + "]";
-    }
-
-    /**
-     * GET /product/:id
-     * @param String id
-     * @return String, json object containing product :id in DB
-     */
-    public static String get(String id) {
-    	System.out.println("Query product: " + id);
-        Jedis jedis = new Jedis(hostname);
-        String res = jedis.get(productsTable + id);
         jedis.close();
         return res;
     }
 
     /**
+     * GET /product/:id
+     * @param String id
+     * @return Product object containing product :id in DB
+     */
+    public static Product get(String id) {
+    	System.out.println("Query product: " + id);
+        Product product = null;
+    	try {
+        	Gson gson = new GsonBuilder().create();
+        	Jedis jedis = new Jedis(hostname);
+        	String res = jedis.get(productsTable + id);
+        	if (res != null) {
+        		product = gson.fromJson(res, Product.class);
+        	}
+        	jedis.close();
+        } catch (Exception e) {
+        	e.printStackTrace();
+        	System.err.println(Products.class.getSimpleName() + e.getMessage());
+        }
+    	
+        return product;
+    }
+
+    /**
      * POST /product
      * @param productJson HTTP Request Body with new product information
-     * @return String, json object containing product with new ID in DB
+     * @return Product object containing product with new ID in DB
      */
-    public static String create(String productJson) {
+    public static Product create(String productJson) {
         System.out.println("Create product: " + productJson); 
         String outputJson = null;
+        Product product = null;
         
         try {
         	Gson gson = new GsonBuilder().create();
-        	Product product = gson.fromJson(productJson, Product.class);
+        	product = gson.fromJson(productJson, Product.class);
         	// connect to redis server
             Jedis jedis = new Jedis(hostname);
             if (jedis.get(productsIndex) == null ) {
@@ -72,33 +89,55 @@ public class Products {
             jedis.set(productsTable + id, outputJson);
             jedis.close();                       
         	 
-       } catch (Exception e) {
+        } catch (Exception e) {
         	e.printStackTrace();
         	System.err.println(Products.class.getSimpleName() + e.getMessage());
         }
         
-        return outputJson;
+        return product;
     }
 
     /**
      * PUT /product/:id
      * @param String id
      * @param productJson HTTP Request Body with new product information
-     * @return String, json object containing update product in DB
+     * @return Product, json object containing update product in DB
      */
-    public static String update(String id, String productJson) {
+    public static Product update(String id, String productJson) {
         System.out.println("Update product: " + id);
         String outputJson = null;
+        Product product = null;        
         Jedis jedis = new Jedis(hostname);
         String res = jedis.get(productsTable + id);
         if (res != null) {
         	// update only if id exists
         	Gson gson = new GsonBuilder().create();
-        	Product product = gson.fromJson(productJson, Product.class);
+        	product = gson.fromJson(productJson, Product.class);
+        	product.setProductId(Integer.parseInt(id));
         	outputJson = gson.toJson(product);
             jedis.set(productsTable + id, outputJson);
         }
         jedis.close();
-        return outputJson;
+        return product;
+    }
+    
+    /**
+     * DELETE /product/:id
+     * @param String id
+     * @return String, json object containing {"result": "Ok" or "Failed"}
+     */
+    public static String delete(String id) {
+        System.out.println("Delete product: " + id);
+        String res = null;
+        Jedis jedis = new Jedis(hostname);
+        Long cnt = jedis.del(productsTable + id);
+        if (cnt == 1) {
+        	// how to report result?
+        	res = "{\"result\": \"Ok\"}";
+        } else {
+        	res = "{\"result\": \"Failed\"}";
+        }
+        jedis.close();
+        return res;
     }
 }
